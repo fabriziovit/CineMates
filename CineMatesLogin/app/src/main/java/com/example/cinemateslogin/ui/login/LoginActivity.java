64fa32abb8b0071000a9d438fccc470f463e6948
Fabrizio Vitale <fabrizio.vitale1@outlook.com>
2020-12-07 18:42:12 +0100

    Aggiunta autenticazione con facebook(Da finire)

--- CineMatesLogin/app/src/main/java/com/example/cinemateslogin/ui/login/LoginActivity.java
+++ CineMatesLogin/app/src/main/java/com/example/cinemateslogin/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 
@@ -24,12 +25,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cinemateslogin.R;
-import com.example.cinemateslogin.ui.login.LoginViewModel;
-import com.example.cinemateslogin.ui.login.LoginViewModelFactory;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
+    private CallbackManager callbackManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,30 @@ public class LoginActivity extends AppCompatActivity {
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
         final TextView passwordDimenticata = findViewById(R.id.password_textView);
+        final LoginButton facebookLogin = findViewById(R.id.login_buttonfb);
+
+        callbackManager = CallbackManager.Factory.create();
+        facebookLogin.setReadPermissions(Arrays.asList("email", "public_profile"));
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<com.facebook.login.LoginResult>() {
+            @Override
+            public void onSuccess(com.facebook.login.LoginResult loginResult)
+            {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
 
         passwordDimenticata.setPaintFlags(passwordDimenticata.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
 
@@ -77,8 +115,11 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 setResult(Activity.RESULT_OK);
 
+
                 //Complete and destroy login activity once successful
                 finish();
+
+
             }
         });
 
@@ -131,5 +172,58 @@ public class LoginActivity extends AppCompatActivity {
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
+        {
+            if(currentAccessToken == null){
+                /*txtEmail.setText("");
+                txtName.setText("");
+                circleImageView.setImageResource(0);
+                Toast.makeText(LoginActivity.this, "Utente disconesso", Toast.LENGTH_LONG).show();*/
+            }else {
+                loaduserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loaduserProfile(AccessToken newAccessToken)
+    {
+        final GraphRequest request =  GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response)
+            {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+                    String image_url = "https://graph.facebook.com/"+id+"/picture?type=normal";
+
+                    /*
+                    txtEmail.setText(email);
+                    txtName.setText(first_name+" "+last_name);
+                    RequestOption requestOption = new RequestOption();
+                    requestOption.dontAnimate();
+                    Glide.with(LoginActivity.this).load(image_url).into(circleImageView);
+                    */
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Bundle parametres = new Bundle();
+        parametres.putString("fields", "first_name,last_name,email,id");
+        request.setParameters(parametres);
+        request.executeAsync();
     }
 }
