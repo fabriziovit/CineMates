ddd797ef931854583af94e43612828f263ce2d9b
GianlucaPignieri <59359832+GianlucaPignieri@users.noreply.github.com>
2020-12-12 17:54:36 +0100

    Registrazione modificata

--- CineMatesLogin/app/src/main/java/com/example/cinemateslogin/ui/login/RegistatiActivity.java
+++ CineMatesLogin/app/src/main/java/com/example/cinemateslogin/ui/login/RegistatiActivity.java
@@ -1,43 +1,67 @@
 package com.example.cinemateslogin.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import com.example.cinemateslogin.R;
 
 public class RegistatiActivity extends AppCompatActivity {
-    TextView risDisponibilita = findViewById(R.id.risDisponibilita_TextView);
-    TextView password = findViewById(R.id.password_textView);
-    TextView confermaPassword = findViewById(R.id.conferma_password_TextField);
-    TextView errorePassword = findViewById(R.id.ErrorePassword_TextView);
-    TextView disponibilta = findViewById(R.id.Disponibilta_TextView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registati);
 
+        final TextView risDisponibilita = findViewById(R.id.risDisponibilita_TextView);
+        final TextView password = findViewById(R.id.password_TextField);
+        final TextView confermaPassword = findViewById(R.id.conferma_password_TextField);
+        final TextView errorePassword = findViewById(R.id.ErrorePassword_TextView);
+        final TextView username = findViewById(R.id.username_TextField);
+        final TextView email = findViewById(R.id.email_TextField);
+        final TextView disponibilita = findViewById(R.id.Disponibilta_TextView);
 
         errorePassword.setVisibility(View.INVISIBLE);
         risDisponibilita.setVisibility(View.INVISIBLE);
-        disponibilta.setVisibility(View.INVISIBLE);
-        onActivity();
-    }
+        disponibilita.setVisibility(View.INVISIBLE);
 
-    protected void onActivity() {
-        confermaPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            confermaPassword.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus) {
+                        if(confermaPassword.getText().toString().equals(password.getText().toString()));
+
+                        else {
+                            errorePassword.setVisibility(View.VISIBLE);
+                        }
+                    }else{
+                        errorePassword.setVisibility(View.INVISIBLE);
+                    }
+                }
+            });
+
+        username.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    if (confermaPassword.getText().toString().equals(password.getText().toString())) {
-                    } else {
-                        errorePassword.setVisibility(View.VISIBLE);
-                    }
+                    //if(QUERY USERNAME NON ESISTENTE){
+                        disponibilita.setVisibility(View.VISIBLE);
+                        risDisponibilita.setVisibility(View.VISIBLE);
+                        risDisponibilita.setText("Disponibile");
+                        risDisponibilita.setTextColor(getResources().getColor(R.color.verdeDis));
+                    /*}else{
+                        disponibilita.setVisibility(View.VISIBLE);
+                        risDisponibilita.setVisibility(View.VISIBLE);
+                        risDisponibilita.setText("Non Disponibile");
+                        risDisponibilita.setTextColor(getResources().getColor(R.color.rossoDis));
+                    }*/
+                }else{
+                    risDisponibilita.setVisibility(View.INVISIBLE);
+                    disponibilita.setVisibility(View.INVISIBLE);
                 }
             }
         });
     }
+
 }
