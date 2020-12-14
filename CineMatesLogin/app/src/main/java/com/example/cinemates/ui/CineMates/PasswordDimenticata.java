52906d886a0e2c17a6599692affd7801704ce6ce
Fabrizio Vitale <fabrizio.vitale1@outlook.com>
2020-12-14 16:27:19 +0100

    Aggiunta password dimenticati

---
+++ CineMatesLogin/app/src/main/java/com/example/cinemates/ui/CineMates/PasswordDimenticata.java
@@ -0,0 +1,16 @@
+package com.example.cinemates.ui.CineMates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cinemates.R;
+
+public class PasswordDimenticata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_dimenticata);
+    }
+}
