436245b38050a1c74089783b7e992b370043ec00
GianlucaPignieri <59359832+GianlucaPignieri@users.noreply.github.com>
2020-12-21 17:15:56 +0100

    Splash screen

---
+++ CineMatesLogin/app/src/main/java/com/example/cinemates/ui/CineMates/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.example.cinemates.ui.CineMates;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.transition.Transition;
+import android.view.View;
+
+import com.example.cinemates.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Transizione(binding);
+    }
+
+
+
+    private void Transizione(ActivitySplashBinding binding){
+
+        binding.motionLayout.addTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int i, int i1) {
+
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int i, int i1, float v) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int i) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int i, boolean b, float v) {
+
+            }
+        });
+    }
+
+}
