09220adbc4bb1c5f51857e92c2af9c061561d8d2
GianlucaPignieri <59359832+GianlucaPignieri@users.noreply.github.com>
2021-01-22 17:55:19 +0100

    cerca film activity fixata

--- CineMatesLogin/app/src/main/java/com/example/cinemates/ui/CineMates/Fragment/SearchFragment.java
+++ CineMatesLogin/app/src/main/java/com/example/cinemates/ui/CineMates/Fragment/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.example.cinemates.ui.CineMates.Fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
@@ -22,16 +24,18 @@ import com.example.cinemates.ui.CineMates.ApiMovie.MovieResearch;
 import com.example.cinemates.ui.CineMates.ApiMovie.SearchMovieApi;
 import com.example.cinemates.ui.CineMates.ItemFilm;
 import com.example.cinemates.ui.CineMates.RecycleViewAdapter_Film;
+import com.example.cinemates.ui.CineMates.SchedaFilmActivity;
 
 import java.util.ArrayList;
 
+import Intefaces.UpdateableFragmentListener;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class SearchFragment extends Fragment implements RecycleViewAdapter_Film.OnClickListener {
+public class SearchFragment extends Fragment implements RecycleViewAdapter_Film.OnClickListener, UpdateableFragmentListener {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -88,53 +92,64 @@ public class SearchFragment extends Fragment implements RecycleViewAdapter_Film.
         });
     }
 
-    private void searchFilm(){
+    private void searchFilm() {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                searchedMovie = new ArrayList<>();
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://api.themoviedb.org")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-                new Thread(()->{
-                    SearchMovieApi searchMovieApi = retrofit.create(SearchMovieApi.class);
-                    Call<MovieResearch> call = searchMovieApi.movieList("3/search/movie?api_key=03941baf012eb2cd38196f9df8751df6&query="+searchField.getText().toString());
-                    call.enqueue(new Callback<MovieResearch>() {
-                        @Override
-                        public void onResponse(Call<MovieResearch> call, Response<MovieResearch> response) {
-                            movieResearch = response.body();
-                            for (Movie movie : movieResearch.getResults()) {
-                                searchedMovie.add(new ItemFilm(movie.getTitle(), ProfileFragment.getBitmapFromdownload(
-                                        "https://image.tmdb.org/t/p/w185" + movie.getPoster_path())));
-                                System.out.println(movie.getTitle());
-                                pop = true;
+                if (searchField.getText().length() != 0) {
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(constraintLayout.getWindowToken(), 0);
+                    searchField.clearFocus();
+                    searchedMovie = new ArrayList<>();
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("https://api.themoviedb.org")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+                    new Thread(() -> {
+                        SearchMovieApi searchMovieApi = retrofit.create(SearchMovieApi.class);
+                        Call<MovieResearch> call = searchMovieApi.movieList("3/search/movie?api_key=03941baf012eb2cd38196f9df8751df6&query=" + searchField.getText().toString());
+                        call.enqueue(new Callback<MovieResearch>() {
+                            @Override
+                            public void onResponse(Call<MovieResearch> call, Response<MovieResearch> response) {
+                                movieResearch = response.body();
+                                for (Movie movie : movieResearch.getResults()) {
+                                    searchedMovie.add(new ItemFilm(movie.getTitle(), ProfileFragment.getBitmapFromdownload(
+                                            "https://image.tmdb.org/t/p/w185" + movie.getPoster_path())));
+                                    update();
+                                    pop = true;
+                                }
+                                if (searchedMovie.size() == 0)
+                                    Toast.makeText(getContext(), "Nessun risultato", Toast.LENGTH_SHORT).show();
                             }
-                        }
 
-                        @Override
-                        public void onFailure(Call<MovieResearch> call, Throwable t) {
-                            Log.e("Errore", "Errore nel caricamento delle api.");
-                        }
-                    });
-                }).start();
-                //while (!pop){}
-                System.out.println("ciao");
-                RecycleViewAdapter_Film recycleViewAdapterFilm = new RecycleViewAdapter_Film(getContext(), searchedMovie, SearchFragment.this::OnClick);
-                recyclerView.setHasFixedSize(true);
-                recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
-                        DividerItemDecoration.HORIZONTAL));
-                recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
-                        DividerItemDecoration.VERTICAL));
-                GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2 , GridLayoutManager.VERTICAL, false);
-                recyclerView.setLayoutManager(gridLayoutManager);
-                recyclerView.setAdapter(recycleViewAdapterFilm);
+                            @Override
+                            public void onFailure(Call<MovieResearch> call, Throwable t) {
+                                Log.e("Errore", "Errore nel caricamento delle api.");
+                            }
+                        });
+                    }).start();
+                    RecycleViewAdapter_Film recycleViewAdapterFilm = new RecycleViewAdapter_Film(getContext(), searchedMovie, SearchFragment.this::OnClick);
+                    recyclerView.setHasFixedSize(true);
+                    recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                            DividerItemDecoration.HORIZONTAL));
+                    recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                            DividerItemDecoration.VERTICAL));
+                    GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false);
+                    recyclerView.setLayoutManager(gridLayoutManager);
+                    recyclerView.setAdapter(recycleViewAdapterFilm);
+                } else
+                    Toast.makeText(getContext(), "Nessun parametro di ricerca!", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
     public void OnClick(int position) {
+        startActivity(new Intent(getActivity(), SchedaFilmActivity.class));
+    }
 
+    @Override
+    public void update() {
+        recyclerView.getAdapter().notifyDataSetChanged();
     }
 }
